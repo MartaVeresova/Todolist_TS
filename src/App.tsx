ef1618f4b89df88cb683f2ec7b8c4c04b66d145e
Marta <martaveresovaa@gmail.com>
2021-04-08 16:28:43 +0300

    Todolist_TS - map, filter, useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Todolist} from './Todolist';
+import {TasksPropsType, Todolist} from './Todolist';
+
+export type FilterValuesType = 'all' | 'active' | 'completed'
 
 function App() {
 
-    const tasksToLearn = [
-        {id: 1, title: 'HTML&CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'React', isDone: false},
-    ]
+    const [tasks, setTasks] = useState<Array<TasksPropsType>>([
+        {id: 1, title: 'HTML&CSS', isDone: true}, //t
+        {id: 2, title: 'JS', isDone: true}, //t
+        {id: 3, title: 'React', isDone: false}, //t
+        {id: 4, title: 'Redux', isDone: false}, //t
+    ])
+    const [filter, setFilter] = useState<FilterValuesType>('all')
 
-    const tasksToBuy = [
-        {id: 4, title: 'Milk', isDone: true},
-        {id: 5, title: 'Beer', isDone: true},
-        {id: 6, title: 'Water', isDone: false},
-    ]
+    function removeTasks(id: number) {
+        const filteredTasks = tasks.filter(t => t.id !== id) //фильтр, пропусти те таски, id-шки которых не равны удаленной id-шке
+        setTasks(filteredTasks) //setTasks - функция, которая меняет данные и вызывается после логической обработки; в параметрах - отфильтрованный массив
+    }
+
+    function changeFilter(value: FilterValuesType) {
+        setFilter(value)
+    }
+
+    let tasksForTodolist = tasks
+    if (filter === 'completed') {
+        tasksForTodolist = tasks.filter(t => t.isDone === true)
+    }
+    if (filter === 'active') {
+        tasksForTodolist = tasks.filter(t => t.isDone === false)
+    }
 
     return (
         <div className="App">
-            <Todolist title={'What to learn'} tasks={tasksToLearn}/>
-            <Todolist title={'What to buy'} tasks={tasksToBuy}/>
+            <Todolist title={'What to learn'}
+                      tasks={tasksForTodolist}
+                      removeTasks={removeTasks}
+                      changeFilter={changeFilter}
+            />
         </div>
     );
 }
