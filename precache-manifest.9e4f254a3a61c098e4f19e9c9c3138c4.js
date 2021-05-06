ad4008df21599793716e38b48ac9d9e33a0baea7
MartaVeresova <MartaVeresovaa@gmail.com>
2021-05-06 14:12:49 +0300

    Updates

---
+++ precache-manifest.9e4f254a3a61c098e4f19e9c9c3138c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a69171a61df9f9208364a7f55ee1582",
+    "url": "/Todolist_TS/index.html"
+  },
+  {
+    "revision": "8fa3153b0d5f2262e0f0",
+    "url": "/Todolist_TS/static/css/main.1313fd01.chunk.css"
+  },
+  {
+    "revision": "8687e3895d0165189ec3",
+    "url": "/Todolist_TS/static/js/2.38b57ddc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todolist_TS/static/js/2.38b57ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa3153b0d5f2262e0f0",
+    "url": "/Todolist_TS/static/js/main.103ba9d0.chunk.js"
+  },
+  {
+    "revision": "ba73e9ab8485d8cb0cd5",
+    "url": "/Todolist_TS/static/js/runtime-main.22b9b94f.js"
+  }
+]);
